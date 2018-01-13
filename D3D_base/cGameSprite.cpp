cac02c2e862aa868c55a0272406696582592b3ae
kys9096 <kys9096@naver.com>
2018-01-13 15:22:09 +0900

    cGameSprite추가
    어떻게 쓰는게 편할지는 계속 고민중입니다.
    
    개선점 있으면 바로 카톡보내주세요.

---
+++ D3D_base/cGameSprite.cpp
@@ -0,0 +1,95 @@
+#include "stdafx.h"
+#include "cGameSprite.h"
+
+
+cGameSprite::cGameSprite()
+	: m_pSprite(NULL)
+	, m_pTexture(NULL)
+	, m_stColor(1.0f, 1.0f, 1.0f, 1.0f)
+	, m_dwSpriteFlags(D3DXSPRITE_ALPHABLEND)
+{
+}
+
+
+cGameSprite::~cGameSprite()
+{
+	this->Destroy();
+}
+
+void cGameSprite::Setup(std::string sFileName)
+{
+	D3DXCreateSprite(g_pD3DDevice, &m_pSprite);
+
+	ZeroMemory(&m_stImageInfo, sizeof(D3DXIMAGE_INFO));
+
+	D3DXCreateTextureFromFileEx(
+		g_pD3DDevice,
+		sFileName.c_str(),
+		D3DX_DEFAULT_NONPOW2,
+		D3DX_DEFAULT_NONPOW2,
+		D3DX_DEFAULT,
+		0,
+		D3DFMT_UNKNOWN,
+		D3DPOOL_MANAGED,
+		D3DX_FILTER_NONE,
+		D3DX_DEFAULT,
+		0,
+		&m_stImageInfo,
+		NULL,
+		&m_pTexture);
+
+	m_vecContentBox.resize(4);
+	m_vecContentBox[0] = D3DXVECTOR2(-m_stImageInfo.Width / 2, -m_stImageInfo.Height / 2); // LT
+	m_vecContentBox[1] = D3DXVECTOR2( m_stImageInfo.Width / 2, -m_stImageInfo.Height / 2); // RT
+	m_vecContentBox[2] = D3DXVECTOR2( m_stImageInfo.Width / 2,  m_stImageInfo.Height / 2); // RB
+	m_vecContentBox[3] = D3DXVECTOR2(-m_stImageInfo.Width / 2,  m_stImageInfo.Height / 2); // LB
+}
+
+void cGameSprite::Render()
+{
+	m_pSprite->SetTransform(&this->GetTransformMatrix());
+
+	m_pSprite->Begin(m_dwSpriteFlags);
+
+	m_pSprite->Draw(m_pTexture, 
+		NULL,
+		&D3DXVECTOR3(m_stImageInfo.Width / 2.0f, m_stImageInfo.Height / 2.0f, 0),
+		&D3DXVECTOR3(0, 0, 0.0f),
+		D3DCOLOR_COLORVALUE(m_stColor.r, m_stColor.g, m_stColor.b, m_stColor.a)
+		//m_stColor
+		);
+
+	m_pSprite->End();
+}
+
+void cGameSprite::Destroy()
+{
+	SAFE_RELEASE(m_pSprite);
+	SAFE_RELEASE(m_pTexture);
+}
+
+std::vector<D3DXVECTOR2> cGameSprite::GetBoundingBox()
+{
+	std::vector<D3DXVECTOR2> vecRet(4);
+	D3DXMATRIXA16 matTrans = this->GetTransformMatrix();
+
+	for (int i = 0; i < 4; i++)
+	{
+		D3DXVec2TransformCoord(&vecRet[i], &m_vecContentBox[i], &matTrans);
+	}
+	return vecRet;
+}
+
+D3DXMATRIXA16 cGameSprite::GetTransformMatrix()
+{
+	D3DXMATRIXA16 matTransform, matS, matR, matT;
+	D3DXMatrixIdentity(&matTransform);
+
+	D3DXMatrixScaling(&matS, m_vScale.x, m_vScale.y, 0.0f);
+	//D3DXMatrixRotationAxis(&matR, &m_vAxis, m_fRotAngle); // use Axis Z
+	D3DXMatrixRotationZ(&matR, m_fRotAngle);
+	D3DXMatrixTranslation(&matT, m_vPos.x, m_vPos.y, 0.0f);
+
+	matTransform = matS * matR * matT;
+	return matTransform;
+}
---
+++ D3D_base/cGameSprite.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class cGameSprite /* RC 방식으로 관리하려면 cGameObject 상속받아서 사용*/
+{
+	LPD3DXSPRITE				m_pSprite;
+	LPDIRECT3DTEXTURE9			m_pTexture;
+	D3DXIMAGE_INFO				m_stImageInfo;
+	SYNTHESIZE_REF(DWORD, m_dwSpriteFlags, SpriteFlag);
+	SYNTHESIZE_REF(D3DXCOLOR, m_stColor); // 0.f~ 1.f
+
+	/* basic data for matrix */
+protected:
+	std::vector<D3DXVECTOR2> m_vecContentBox;
+
+	SYNTHESIZE_REF(D3DXVECTOR2, m_vPos, Position);
+	//SYNTHESIZE_REF(D3DXVECTOR3, m_vAxis, Axis); // use Axis(0,0,1)
+	SYNTHESIZE_REF(float, m_fRotAngle, RotAngle); // Z angle
+	SYNTHESIZE_REF(D3DXVECTOR2, m_vScale, Scale);
+
+public:
+	cGameSprite();
+	~cGameSprite();
+
+	void Setup(std::string sFileName);
+	void Render();
+	void Destroy();
+
+	std::vector<D3DXVECTOR2> GetBoundingBox();
+	D3DXMATRIXA16 GetTransformMatrix();
+
+	D3DXIMAGE_INFO GetImageInfo() { return m_stImageInfo; }
+};
+
